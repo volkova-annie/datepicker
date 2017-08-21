4d5fb9921d5383805959696b4b1c396713e72c71
Volkova Annie <annie_v@bk.ru>
2017-08-21 18:39:56 +0300

    update components

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,52 +1,19 @@
 <template>
   <div id='app'>
-    <p id='hello'>Hello, world!</p>
-    <input type="text" name="" value="" id="date-picker">
-    <!-- <div class='date-picker'>
-      <datepicker language='ru' :format='datePickerOptions.format'></datepicker>
-    </div> -->
+    <date-picker></date-picker>
   </div>
 </template>
 
 <script>
-// import Datepicker from 'vue-material-datepicker'
-// import Datepicker from 'vuejs-datepicker'
-import $ from 'jquery'
-import Zebra_DatePicker from 'zebra_datepicker'
-// import 'zebra_datepicker/dist/css/default/zebra_datepicker.min.css'
-
-
-var state = {
-    date1: new Date()
-}
+import DatePicker from './DatePicker.vue'
 
 export default {
   name: 'app',
-  mounted: function(){
-    $('#date-picker').Zebra_DatePicker({
-      // direction: [false, thisYear - 100 + '-01-01'],
-      // start_date: thisYear - 16 + '-01-01',
-      // view: 'years',
-      // offset: [0,0],
-      // show_icon: false
-    })
+  components: {
+    DatePicker
   }
-  // components: {
-  //   Datepicker
-  // },
-  // data () {
-  //   return {
-  //     datePickerOptions: {
-  //       format: 'dd.MM.yyyy',
-  //       closeButton: true
-  //     }
-  //   }
-  // }
 }
 </script>
-<style>
-  #date-picker, #hello {
-    color: red;
-  }
 
+<style>
 </style>
--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,12 +1,127 @@
 <template>
-  <div id='date-picker'>
+    <div class='container__datepicker'>
+      <h4 class='datepicker-title'>Период:</h4>
+      <datepicker
+        monday-first
+        class='datepicker date-start'
+        language='ru'
+        :format='format'
+        placeholder='Выберите дату...'
+        v-model='start'
+        @selected='onSelectStart'
+        :highlighted='state.highlighted'
+        :disabled='disabled'
+        v-bind:full-month-name='true'
+      ></datepicker>
 
-  </div>
+      <datepicker
+        class='datepicker date-end'
+        language='ru'
+        :format='format'
+        placeholder='Выберите дату...'
+        monday-first
+        v-model='end'
+        @selected='onSelectEnd'
+        :highlighted='state.highlighted'
+        :disabled='disabled'
+        v-bind:full-month-name='true'
+      ></datepicker>
+    </div>
 </template>
+
 <script>
+import Datepicker from 'vuejs-datepicker'
 
+var state = {
+  disabled: {
+    to: new Date()
+  },
+  highlighted: {
+    dates: [
+      new Date()
+    ]
+  }
+}
 
+export default {
+  components: {
+    Datepicker
+  },
+  data: function () {
+    return {
+      format: 'dd MMM yyyy',
+      state: state,
+      start: null,
+      closeButton: true,
+      end: null
+    }
+  },
+  computed: {
+    disabled: function() {
+      const yesterday = new Date() - 84600
+      return {
+        to: new Date(yesterday)
+      }
+    }
+  },
+  methods: {
+    onSelectStart(val) {
+      if (this.end && this.end < val) {
+        this.end = val
+      }
+    },
+    onSelectEnd(val) {
+      if (this.start && this.start > val) {
+          this.start = val
+      }
+    }
+  }
+}
 </script>
 <style>
+  .container__datepicker {
+    display: flex;
+    font-family: Roboto;
+  }
+  .datepicker {
+    color: #5d5d5d;
+    margin: 10px;
+  }
+  .container__datepicker > .datepicker > div > input {
+    width: 210px;
+    text-align: center;
+  }
+  .container__datepicker > .datepicker > div > input::placeholder {
+    font-style: italic;
+    text-align: left;
+    padding-left: 5px;
+  }
+  .container__datepicker > .datepicker > div > input:focus {
+    border-color: #69a3ea;
+  }
+  .vdp-datepicker__calendar {
+    width: 210px;
+  }
+  .vdp-datepicker__calendar .cell, .vdp-datepicker__calendar > header, .prev, .up, .next {
+    height: 35px;
+  }
+  .vdp-datepicker__calendar > header, .prev, .up, .next {
+    color: #ffffff;
+    background-color: #3f51b5;
+  }
+  .vdp-datepicker__calendar header .prev:not(.disabled):hover,
+  .vdp-datepicker__calendar header .next:not(.disabled):hover,
+  .vdp-datepicker__calendar > header .up:not(.disabled):hover {
+    color: #ffffff;
+    background-color: #3f51b5;
+  }
+  .weekend {
+    color: red;
+  }
+  .datepicker-title {
+    margin: 0;
+    display: flex;
+    align-items: center;
+  }
 
 </style>
